7dac22d4b828ebd43807a3f9c6a513fb53646b96
dababy069 <83238236+dababy069@users.noreply.github.com>
2021-04-27 17:04:03 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,67 +1,26 @@
 from django.db import models
-from django.urls import reverse
-from django.conf import settings
-import os
-
 
 # Create your models here.
 
-STATUS_CHOICES = (('notavailable', 'Notavailable'),('available', 'Available'),)
-
-RATING_CHOICES = ((1, "★☆☆☆☆"), (2, "★★☆☆☆"), (3, "★★★☆☆"), (4, "★★★★☆"), (5, "★★★★★"))
-
-class AvailableManager(models.Manager):
-	def get_queryset(self):
-		return super(AvailableManager, self).get_queryset() .filter(status='available')
-
-class City(models.Model):
+class Chief_of_staff(models.Model):
 	name = models.CharField(max_length=200, db_index=True)
-	active = models.BooleanField(default=True)
-	def __str__(self):
-		return self.name
-	objects = models.Manager() # The default manager.
-	available = AvailableManager() # Our custom manager.
-
-class Category(models.Model):
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, unique=True)
+	role = models.CharField(max_length=200, db_index=True)
+	hobby = models.CharField(max_length=200, db_index=True)
+	contact = models.CharField(max_length=200, db_index=True)
+	facebook = models.CharField(max_length=200, db_index=True)
+	twitter = models.CharField(max_length=200, db_index=True)
+	instagram = models.CharField(max_length=200, db_index=True)
+	pictur = models.ImageField(upload_to='principals/%Y/%m/%d', blank=True)
+	active = models.BooleanField(default=False)
 	class Meta:
-		ordering = ('name',)
-		verbose_name = 'category'
-		verbose_name_plural = 'categories'
-
+		ordering = ('-role',)
 	def __str__(self):
 		return self.name
-	def get_absolute_url(self):
-		return reverse('store:product_list_by_category', args=[self.slug])
 
+class Aboutus(models.Model):
+	title = models.CharField(max_length=200)
+	detail = models.TextField(blank=True)
 
-class Product(models.Model):
-	category = models.ForeignKey(Category, related_name='products',	on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, db_index=True)
-	image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-	description = models.TextField(blank=True)
-	price = models.DecimalField(max_digits=10, decimal_places=2)
-	rating = models.PositiveIntegerField(choices=RATING_CHOICES, blank=True, null=True)
-	status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='notavailable')
-	expiredate = models.DateTimeField()
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	class Meta:
-		ordering = ('name', '-created',)
-		index_together = (('id', 'slug'),)
 	def __str__(self):
-		return self.name
-	def get_absolute_url(self):
-		return reverse('store:product_detail', args=[self.id, self.slug])
-	def get_rating_percentage(self):
-		return self.rating * 20 if self.rating is not None else None
-
-
-	objects = models.Manager() # The default manager.
-	available = AvailableManager() # Our custom manager.
+		return self.title
 	
-
-
-
